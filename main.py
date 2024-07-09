757f417c282a8defe981e6d9122d54705862e605
Im-Stivik <mogil.sharon@gmail.com>
2024-07-09 16:27:53 +0300

    made a basic fast api server

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
+from fastapi import FastAPI, HTTPException, Request
+from pydantic import BaseModel
+
 from src.MinecaftServerManager import MinecraftServerInstance
 
-server_path = 'server/'
-
+app = FastAPI()
+server_path = 'server/'  # Adjust the path to your Minecraft server directory
 server_instance = MinecraftServerInstance(server_path)
-server_instance.start_server()
 
-while True:
-    command = input('Enter command: ')
-    if command == 'output':
-        output = server_instance.read_output()
-        print(output)
-    elif command == 'tail':
-        print('how many lines to read?')
-        lines = int(input())
-        output = server_instance.read_tail(lines)
-        print(output)
-    elif command == 'stop':
-        server_instance.stop_server()
-        break
-    elif command == 'restart':
-        server_instance.restart_server()
-    elif command == 'recreate world':
-        server_instance.recreate_world()
-    else:
-        server_instance.execute_command(command)
+
+class Command(BaseModel):
+    command: str
+
+
+@app.post("/server/start")
+async def start_server():
+    server_instance.start_server()
+    return {"message": "Server started"}
+
+
+@app.post("/server/command")
+async def send_command(command: Command):
+    server_instance.execute_command(command.command)
+    return {"message": f"Command '{command.command}' sent"}
+
+
+@app.get("/server/output")
+async def read_output():
+    output = server_instance.read_output()
+    return {"output": output}
+
+
+@app.post("/server/stop")
+async def stop_server():
+    server_instance.stop_server()
+    return {"message": "Server stopped"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
